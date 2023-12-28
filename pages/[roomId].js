aec96443bf05381c3a7a67809e98f8055a0175ed
Akshat <akshatjangir1710@gmail.com>
2023-12-28 16:00:53 +0530

    application working

--- pages/[roomId].js
+++ pages/[roomId].js
@@ -1,9 +1,264 @@
-import { useEffect } from "react";
+import { useEffect, useState, useCallback } from "react";
+import { cloneDeep } from "lodash";
+
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+
 import { useSocket } from "@/context/socket";
 import usePeer from "@/hooks/usePeer";
+import useMediaStream from "@/hooks/useMediaStream";
+import usePlayer from "@/hooks/usePlayer";
+
+import Player from "@/components/Player";
+import Bottom from "@/components/Bottom";
+import CopySection from "@/components/copySection";
+
+import styles from "@/styles/room.module.css";
+import { useRouter } from "next/router";
+
 const Room = () => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
   const socket = useSocket();
-  const { peer, myTd } = usePeer();
+  const { roomId } = useRouter().query;
+  const { peer, myId } = usePeer();
+  const { stream } = useMediaStream();
+  const {
+    players,
+    setPlayers,
+    playerHighlighted,
+    nonHighlightedPlayers,
+    toggleAudio,
+    toggleVideo,
+    leaveRoom,
+  } = usePlayer(myId, roomId, peer);
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    if (!socket || !peer || !stream) return;
+    const handleUserConnected = (newUser) => {
+      console.log(`user connected in room with userId ${newUser}`);
+
+      const call = peer.call(newUser, stream);
+
+      call.on("stream", (incomingStream) => {
+        console.log(`incoming stream from ${newUser}`);
+        setPlayers((prev) => ({
+          ...prev,
+          [newUser]: {
+            url: incomingStream,
+            muted: true,
+            playing: true,
+          },
+        }));
+
+        setUsers((prev) => ({
+          ...prev,
+          [newUser]: call,
+        }));
+      });
+    };
+    socket.on("user-connected", handleUserConnected);
+
+    return () => {
+      socket.off("user-connected", handleUserConnected);
+    };
+  }, [peer, setPlayers, socket, stream]);
+
+  useEffect(() => {
+    if (!socket) return;
+    const handleToggleAudio = (userId) => {
+      console.log(`user with id ${userId} toggled audio`);
+      setPlayers((prev) => {
+        const copy = cloneDeep(prev);
+        copy[userId].muted = !copy[userId].muted;
+        return { ...copy };
+      });
+    };
+
+    const handleToggleVideo = (userId) => {
+      console.log(`user with id ${userId} toggled video`);
+      setPlayers((prev) => {
+        const copy = cloneDeep(prev);
+        copy[userId].playing = !copy[userId].playing;
+        return { ...copy };
+      });
+    };
+
+    const handleUserLeave = (userId) => {
+      console.log(`user ${userId} is leaving the room`);
+      users[userId]?.close();
+      const playersCopy = cloneDeep(players);
+      delete playersCopy[userId];
+      setPlayers(playersCopy);
+    };
+    socket.on("user-toggle-audio", handleToggleAudio);
+    socket.on("user-toggle-video", handleToggleVideo);
+    socket.on("user-leave", handleUserLeave);
+    return () => {
+      socket.off("user-toggle-audio", handleToggleAudio);
+      socket.off("user-toggle-video", handleToggleVideo);
+      socket.off("user-leave", handleUserLeave);
+    };
+  }, [players, setPlayers, socket, users]);
+
+  useEffect(() => {
+    if (!peer || !stream) return;
+    peer.on("call", (call) => {
+      const { peer: callerId } = call;
+      call.answer(stream);
+
+      call.on("stream", (incomingStream) => {
+        console.log(`incoming stream from ${callerId}`);
+        setPlayers((prev) => ({
+          ...prev,
+          [callerId]: {
+            url: incomingStream,
+            muted: true,
+            playing: true,
+          },
+        }));
+
+        setUsers((prev) => ({
+          ...prev,
+          [callerId]: call,
+        }));
+      });
+    });
+  }, [peer, setPlayers, stream]);
+
+  useEffect(() => {
+    if (!stream || !myId) return;
+    console.log(`setting my stream ${myId}`);
+    setPlayers((prev) => ({
+      ...prev,
+      [myId]: {
+        url: stream,
+        muted: true,
+        playing: true,
+      },
+    }));
+  }, [myId, setPlayers, stream]);
+
+  return (
+    <>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "#000000",
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.8,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <div className={styles.activePlayerContainer}>
+        {playerHighlighted && (
+          <Player
+            url={playerHighlighted.url}
+            muted={playerHighlighted.muted}
+            playing={playerHighlighted.playing}
+            isActive
+          />
+        )}
+      </div>
+      <div className={styles.inActivePlayerContainer}>
+        {Object.keys(nonHighlightedPlayers).map((playerId) => {
+          const { url, muted, playing } = nonHighlightedPlayers[playerId];
+          return (
+            <Player
+              key={playerId}
+              url={url}
+              muted={muted}
+              playing={playing}
+              isActive={false}
+            />
+          );
+        })}
+      </div>
+      <CopySection roomId={roomId} />
+      <Bottom
+        muted={playerHighlighted?.muted}
+        playing={playerHighlighted?.playing}
+        toggleAudio={toggleAudio}
+        toggleVideo={toggleVideo}
+        leaveRoom={leaveRoom}
+      />
+    </>
+  );
 };
 
 export default Room;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import { v4 as uuidv4 } from "uuid";
 import { useRouter } from "next/navigation";
 import styles from "@/styles/Home.module.css";
 import { useState } from "react";
+import React from "react";
+
 export default function Home() {
   const router = useRouter();
   const [roomId, setRoomId] = useState("");
@@ -19,18 +21,20 @@ export default function Home() {
     }
   };
   return (
-    <div className={styles.homeContainer}>
-      <h1>Google Meet clone</h1>
-      <div className={styles.enterRoom}>
-        <input
-          placeholder="Enter Room Id"
-          value={roomId}
-          onChange={(e) => setRoomId(e?.target?.value)}
-        />
-        <button onClick={joinRoom}>Join Room</button>
+    <>
+      <div className={styles.homeContainer}>
+        <h1>Google Meet clone</h1>
+        <div className={styles.enterRoom}>
+          <input
+            placeholder="Enter Room Id"
+            value={roomId}
+            onChange={(e) => setRoomId(e?.target?.value)}
+          />
+          <button onClick={joinRoom}>Join Room</button>
+        </div>
+        <span className={styles.separatorText}>OR</span>
+        <button onClick={createAndJoin}>Create New Room</button>
       </div>
-      <span className={styles.separatorText}>OR</span>
-      <button onClick={createAndJoin}>Create New Room</button>
-    </div>
+    </>
   );
 }
