6ca008632f1e4cc156f963fe4d5491d6cb8c1ead
Akshat <akshatjangir1710@gmail.com>
2023-12-28 23:57:12 +0530

    ui-changes

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,16 @@ import { useState } from "react";
 import React from "react";
 import App from "./_app";
 import Hero from "@/components/Hero";
+import { NextUIProvider } from "@nextui-org/react";
+import Navbar from "@/components/Navbar";
+import { motion } from "framer-motion";
+
+const visible = { opacity: 1, y: 0, transition: { duration: 0.5 } };
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 10 },
+  visible,
+};
 
 export default function Home() {
   const router = useRouter();
@@ -24,41 +34,71 @@ export default function Home() {
   };
   return (
     <>
-      <div className="flex items-center justify-between p-8 md:p-16 bg-gradient-to-r from-gray-700 via-gray-900 to-black ">
-        {/* Left side */}
-        <div className="flex flex-col max-w-md">
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4 text-blue-500">
-            Engage customers with{" "}
-            <span className="text-red-600">one-Click </span> video call{" "}
-          </h1>
-          <div className="flex flex-col mb-4">
-            <label htmlFor="roomId" className="text-gray-300 mb-2">
-              Enter Room ID:
-            </label>
-            <input
-              className="px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-              value={roomId}
-              onChange={(e) => setRoomId(e?.target?.value)}
-            />
-          </div>
-          <button
-            className="bg-blue-500 text-white px-6 py-3 rounded-md mb-4 hover:bg-blue-600"
-            onClick={joinRoom}
-          >
-            Join Room
-          </button>
-          <button
-            className="bg-green-700 text-white px-6 py-3 rounded-md hover:bg-green-600"
-            onClick={createAndJoin}
-          >
-            Create a New Room
-          </button>
-        </div>
+      <NextUIProvider>
+        <Navbar />
 
-        <div className="hidden md:block">
-          <Hero />
-        </div>
-      </div>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          exit={{ opacity: 0, transition: { duration: 1 } }}
+          variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
+          className="flex items-center justify-between p-8 md:p-16 bg-white "
+        >
+          {/* Left side */}
+          <div className="flex flex-col max-w-md">
+            <motion.h1
+              variants={{
+                hidden: { opacity: 0, y: -20 },
+                visible,
+              }}
+              className="text-4xl md:text-5xl lg:text-5xl font-bold mb-4 text-blue-500 font-poppins"
+            >
+              Engage customers with{" "}
+              <span className="text-red-600">one-Click </span> video call{" "}
+            </motion.h1>
+            <motion.div
+              variants={{
+                hidden: { opacity: 0, y: -20 },
+                visible,
+              }}
+              className="flex flex-col mb-4"
+            >
+              <label htmlFor="roomId" className="text-gray-700 mb-2">
+                Enter Room ID:
+              </label>
+              <input
+                className="px-4 py-2 border border-gray-600 rounded-md focus:outline-none focus:border-gray-500"
+                value={roomId}
+                onChange={(e) => setRoomId(e?.target?.value)}
+              />
+            </motion.div>
+            <motion.button
+              variants={{
+                hidden: { opacity: 0, y: -20 },
+                visible,
+              }}
+              className="bg-blue-500 text-white px-6 py-3 rounded-md mb-4 hover:bg-blue-600"
+              onClick={joinRoom}
+            >
+              Join Room
+            </motion.button>
+            <motion.button
+              variants={{
+                hidden: { opacity: 0, y: -20 },
+                visible,
+              }}
+              className="bg-green-700 text-white px-6 py-3 rounded-md hover:bg-green-600"
+              onClick={createAndJoin}
+            >
+              Create a New Room
+            </motion.button>
+          </div>
+
+          <div className="hidden md:block">
+            <Hero />
+          </div>
+        </motion.div>
+      </NextUIProvider>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
       colors: {
         prime: "#212529",
         secondary: "#343a40",
