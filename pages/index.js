e925d85e5dcc2df5b67d5297f6bbb64c6b592392
Akshat <akshatjangir1710@gmail.com>
2023-12-28 19:20:43 +0530

    ui improve

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import { useRouter } from "next/navigation";
 import styles from "@/styles/Home.module.css";
 import { useState } from "react";
 import React from "react";
+import App from "./_app";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   const router = useRouter();
@@ -22,18 +24,40 @@ export default function Home() {
   };
   return (
     <>
-      <div className={styles.homeContainer}>
-        <h1>Google Meet clone</h1>
-        <div className={styles.enterRoom}>
-          <input
-            placeholder="Enter Room Id"
-            value={roomId}
-            onChange={(e) => setRoomId(e?.target?.value)}
-          />
-          <button onClick={joinRoom}>Join Room</button>
+      <div className="flex items-center justify-between p-8 md:p-16 bg-gradient-to-r from-gray-700 via-gray-900 to-black ">
+        {/* Left side */}
+        <div className="flex flex-col max-w-md">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4 text-blue-500">
+            Engage customers with{" "}
+            <span className="text-red-600">one-Click </span> video call{" "}
+          </h1>
+          <div className="flex flex-col mb-4">
+            <label htmlFor="roomId" className="text-gray-300 mb-2">
+              Enter Room ID:
+            </label>
+            <input
+              className="px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+              value={roomId}
+              onChange={(e) => setRoomId(e?.target?.value)}
+            />
+          </div>
+          <button
+            className="bg-blue-500 text-white px-6 py-3 rounded-md mb-4 hover:bg-blue-600"
+            onClick={joinRoom}
+          >
+            Join Room
+          </button>
+          <button
+            className="bg-green-700 text-white px-6 py-3 rounded-md hover:bg-green-600"
+            onClick={createAndJoin}
+          >
+            Create a New Room
+          </button>
         </div>
-        <span className={styles.separatorText}>OR</span>
-        <button onClick={createAndJoin}>Create New Room</button>
+
+        <div className="hidden md:block">
+          <Hero />
+        </div>
       </div>
     </>
   );
